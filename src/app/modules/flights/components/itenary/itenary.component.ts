4c81c6bf3d283d9a0d09cea5a292205132d1c18d
TirruM <ythriveni65@gmail.com>
2020-08-01 19:39:53 +0530

    Integrated flight search api with static data and prepared combo mapping Type Itineraries.

--- src/app/modules/flights/components/itenary/itenary.component.ts
+++ src/app/modules/flights/components/itenary/itenary.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-itenary',
   templateUrl: './itenary.component.html',
   styleUrls: ['./itenary.component.css']
 })
-export class ItenaryComponent implements OnInit {
-
+export class ItenaryComponent implements OnInit, OnChanges {
+  @Input() itinerary;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes): void {
+    this.itinerary = changes.itinerary.currentValue;
   }
 
 }
--- src/app/modules/flights/pages/flight-search-results/flight-search-results.component.ts
+++ src/app/modules/flights/pages/flight-search-results/flight-search-results.component.ts
@@ -10,28 +10,55 @@ import { FlightSearchRes } from '../../models/flightSearchRes';
 })
 export class FlightSearchResultsComponent implements OnInit {
   public flightSearchRequest: FlightSearchReq = {};
+  public comboFlightSearchResults = [];
+  public flightMappingType: string;
   constructor(private flightApiService: FlightApiService) {
-    
+
   }
 
   ngOnInit() {
     this.flightSearchRequest = JSON.parse(localStorage.getItem("flight-search"));
-    console.log("flight search request in flight search results component",this.flightSearchRequest);
+    console.log("flight search request in flight search results component", this.flightSearchRequest);
     this.getFlightSearchResults();
   }
 
+  /* Get Flight Search Results  */
   public getFlightSearchResults(): void {
-    console.log("calling flight search results api call ");
-    this.flightApiService.request("post", "api/FlightBusiness/AirSearch", this.flightSearchRequest ).subscribe((flightSearchRes: FlightSearchRes) => {
-      console.log(flightSearchRes);
-      if (flightSearchRes.errors.length === 0) {
+    this.flightApiService.request("post", "api/FlightBusiness/AirSearch", this.flightSearchRequest).subscribe((flightSearchRes: FlightSearchRes) => {
+/*  */      if (flightSearchRes.errors.length === 0) {
+        this.flightMappingType = flightSearchRes.itineraries.mappingType;
         if (flightSearchRes.itineraries.mappingType === "Combined") {
-
+          this.prepareCombinedSearcfhResults(flightSearchRes);
         } else {
 
         }
       }
     })
+  }
+
+  /* Prepare Combined Flight SearchResults */
+  public prepareCombinedSearcfhResults(flightSearchRes): void {
+    flightSearchRes.itineraries.combo.map(itineray => {
+      let obj = itineray;
+      let segments = [];
+      obj.segmentMapping.forEach(seg => {
+        var segInfo = [];
+        seg.forEach(segItem => {
+          segInfo.push(flightSearchRes.segments.find(seg => seg.segId === segItem));
+        })
+        obj["segments"] = segInfo;
+      })
+      obj["fares"] = flightSearchRes.fares.find(fare => fare.fareId === itineray.fareMapping);
+      return obj;
+    })
+    this.comboFlightSearchResults = flightSearchRes.itineraries.combo;
+    console.log(this.comboFlightSearchResults);
+  }
+
+
+  /* Prepare Separated Flight SearchResults */
+  public prepareSeparatedSearcfhResults(): void {
+
   }
 
 }
